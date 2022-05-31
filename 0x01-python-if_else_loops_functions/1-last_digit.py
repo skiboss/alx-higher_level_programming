0df8bd686cba4045e87d484392f8ecaa117a36dc
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-05-31 16:24:58 -0700

    Add: Python file that prints last digit of a randomly generated number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+no = str(number)
+last_digit = int(no[-1])
+result = f"Last digit of {number} is {last_digit}"
+if last_digit > 5:
+    print(f"{result} and is greater than 5")
+elif last_digit == 0:
+    print(f"{result} and is 0")
+elif last_digit < 6 and last_digit != 0:
+    print(f"{result} and is less than 6 and not 0")
