0734b608d0ff749efd0a09d03a69b69bb163a90f
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-06-02 20:16:14 -0700

    Add: add_0.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return. a + b
+    """
+    return (a + b)
