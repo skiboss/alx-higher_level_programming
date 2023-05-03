cd769a76618a587fac873232edefe216d61d44ec
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2023-05-03 05:54:03 +0100

    Fix comment line requirement

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes URL with GET and sends header variable X-School-User-Id
-# with the value 98
+# Script that takes URL with GET and sends header variable X-School-User-Id with the value 98
 curl -s -X GET -H "X-School-User-Id: 98" "$1"
